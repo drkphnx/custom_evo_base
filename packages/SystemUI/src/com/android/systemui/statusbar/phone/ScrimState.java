b8cc181c7f54f15a7ad1eaab2fbec3ffa426ada0
Danny Lin <danny@kdrag0n.dev>
2021-10-11 19:24:58 -0700

    SystemUI: Follow light/dark theme in quick settings
    Android 12's dual-tone style where the quick settings panel is always
    dark makes the light theme look like a second-class citizen. Pure black
    also looks out-of-place next to QS tiles and the notification panel
    because dynamic themes don't affect it at all.
    
    Revert to the ~Beta 1 style where quick settings used the same theme as
    the notification shade.
    
    - colorAccentPrimary has been replaced with colorAccent for contrast in
      light mode, because colorAccentPrimary is system_accent1_100 (dark
      accent color)
    - Footer chips have been converted to surfaces (similar to QS tiles and
      notifications), which makes more sense with the new style
    - The QS background is now the same as the notification shade background
      in both light and dark modes
    
    Demo screenshots (with dual-tone commit):
    https://twitter.com/kdrag0n/status/1445922541218922496
    
    [kdrag0n: ported to 12L]
    
    Change-Id: I3d45b72f0f119e100505409d178ab8f698993881

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimState.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/ScrimState.java
@@ -120,6 +120,7 @@ public enum ScrimState {
             mBehindAlpha = mClipQsScrim ? mQSClipScrimAlpha : mDefaultScrimAlpha;
             mBehindTint = Color.TRANSPARENT;
             mNotifAlpha = mClipQsScrim ? mQSClipScrimAlpha : 0;
+            mBehindTint = Color.TRANSPARENT;
             mNotifTint = Color.TRANSPARENT;
             mFrontAlpha = 0f;
         }
@@ -142,7 +143,7 @@ public enum ScrimState {
             mBehindAlpha = mClipQsScrim ? mQSClipScrimAlpha : mDefaultScrimAlpha;
             mNotifAlpha = mClipQsScrim ? mQSClipScrimAlpha : mDefaultScrimAlpha;
             mFrontAlpha = 0f;
-            mBehindTint = Color.BLACK;
+            mBehindTint = Color.TRANSPARENT;
 
             if (mClipQsScrim) {
                 updateScrimColor(mScrimBehind, mQSClipScrimAlpha /* alpha */, Color.TRANSPARENT);
@@ -152,7 +153,7 @@ public enum ScrimState {
         // to make sure correct color is returned before "prepare" is called
         @Override
         public int getBehindTint() {
-            return Color.BLACK;
+            return Color.TRANSPARENT;
         }
     },
 
@@ -243,17 +244,17 @@ public enum ScrimState {
             mAnimateChange = !mLaunchingAffordanceWithPreview && !fromAod;
 
             mFrontTint = Color.TRANSPARENT;
-            mBehindTint = Color.BLACK;
+            mBehindTint = Color.TRANSPARENT;
             mBlankScreen = false;
 
             if (previousState == ScrimState.AOD) {
                 // Set all scrims black, before they fade transparent.
                 updateScrimColor(mScrimInFront, 1f /* alpha */, Color.BLACK /* tint */);
-                updateScrimColor(mScrimBehind, 1f /* alpha */, Color.BLACK /* tint */);
+                updateScrimColor(mScrimBehind, 1f /* alpha */, Color.TRANSPARENT /* tint */);
 
                 // Scrims should still be black at the end of the transition.
                 mFrontTint = Color.BLACK;
-                mBehindTint = Color.BLACK;
+                mBehindTint = Color.TRANSPARENT;
                 mBlankScreen = true;
             }
 
